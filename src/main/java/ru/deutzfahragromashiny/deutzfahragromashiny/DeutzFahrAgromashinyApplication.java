26b498c35523506028b57eeac776b7976b926e81
Rostoslav Telitsin <rostislav.telitsin@gmail.com>
2022-01-24 19:38:05 +0700

    init

---
+++ src/main/java/ru/deutzfahragromashiny/deutzfahragromashiny/DeutzFahrAgromashinyApplication.java
@@ -0,0 +1,13 @@
+package ru.deutzfahragromashiny.deutzfahragromashiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeutzFahrAgromashinyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeutzFahrAgromashinyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/deutzfahragromashiny/deutzfahragromashiny/DeutzFahrAgromashinyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.deutzfahragromashiny.deutzfahragromashiny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeutzFahrAgromashinyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
